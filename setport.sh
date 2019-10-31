4fbf5a17ac309257de9377f546e7f6b838934e05
Feng <zefeng@live.cn>
2019-10-31 20:20:56 +0800

    update

--- setport.sh
+++ setport.sh
@@ -2,55 +2,32 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-sh_ver="2.0.38"
-filepath=$(cd "$(dirname "$0")"; pwd)
-file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 ssr_folder="/usr/local/shadowsocksr"
-ssr_ss_file="${ssr_folder}/shadowsocks"
-config_file="${ssr_folder}/config.json"
+jq_file="${ssr_folder}/jq"
 config_folder="/etc/shadowsocksr"
 config_user_file="${config_folder}/user-config.json"
-ssr_log_file="${ssr_ss_file}/ssserver.log"
-Libsodiumr_file="/usr/local/lib/libsodium.so"
-Libsodiumr_ver_backup="1.0.13"
-Server_Speeder_file="/serverspeeder/bin/serverSpeeder.sh"
-LotServer_file="/appex/bin/serverSpeeder.sh"
-BBR_file="${file}/bbr.sh"
-jq_file="${ssr_folder}/jq"
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-Separator_1="——————————————————————————————"
+port=`${jq_file} '.server_port' ${config_user_file}`
 
-# 设置 配置信息
-Set_config_port(){
-	while true
-	do
-	echo -e "请输入要设置的ShadowsocksR账号 端口"
-	read -e -p "(默认: 2333):" ssr_port
-	[[ -z "$ssr_port" ]] && ssr_port="2333"
-	echo $((${ssr_port}+0)) &>/dev/null
-	if [[ $? == 0 ]]; then
-		if [[ ${ssr_port} -ge 1 ]] && [[ ${ssr_port} -le 65535 ]]; then
-			echo && echo ${Separator_1} && echo -e "	端口 : ${Green_font_prefix}${ssr_port}${Font_color_suffix}" && echo ${Separator_1} && echo
-			break
-		else
-			echo -e "${Error} 请输入正确的数字(1-65535)"
+# 读取 IP配置信息
+Get_IP(){
+	ip=$(wget -qO- -t1 -T2 ipinfo.io/ip)
+	if [[ -z "${ip}" ]]; then
+		ip=$(wget -qO- -t1 -T2 api.ip.sb/ip)
+		if [[ -z "${ip}" ]]; then
+			ip=$(wget -qO- -t1 -T2 members.3322.org/dyndns/getip)
+			if [[ -z "${ip}" ]]; then
+				ip="VPS_IP"
+			fi
 		fi
-	else
-		echo -e "${Error} 请输入正确的数字(1-65535)"
 	fi
-	done
 }
 
+# 设置 配置信息
 Modify_config_port(){
 	ls_date=`date +%m%d`
 	getTime=3${ls_date}
-	port=`${jq_file} '.server_port' ${config_user_file}`
 	sed -i 's/"server_port": '"$(echo ${port})"'/"server_port": '"$(echo ${getTime})"'/g' ${config_user_file}
 }
-
 
 # 重启服务
 check_pid(){
@@ -65,6 +42,8 @@ Restart_SSR(){
 }
 
 # start
-# Set_config_port
+Get_IP
 Modify_config_port
 Restart_SSR
+
+# 调用API
